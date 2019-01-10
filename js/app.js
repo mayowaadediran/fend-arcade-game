75e242bc503ac0dc5174a2aed5c2a13d49020207
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-01-10 12:59:16 +0100

    change classes and methods to ES6 syntax

--- js/app.js
+++ js/app.js
@@ -1,22 +1,12 @@
-// Enemies our player must avoid
-var Enemy = function(x , y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    //randomly assign speed 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+class Enemy  {
+  constructor(x, y, speed) {
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.sprite = 'images/enemy-bug.png'
+  }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  update(dt) {
     if (this.x < 505) {
       this.x += 200 * this.speed * dt;
     }
@@ -24,68 +14,75 @@ Enemy.prototype.update = function(dt) {
       this.x = -100;
     }
 
-  for (let enemy of allEnemies) {
-    if (this.y === player.y && this.x + 40 >= player.x && this.x - 40 <= player.x) {
-      player.resetPlayer();
+    for (let enemy of allEnemies) {
+      if (this.y === player.y && this.x + 40 >= player.x && this.x - 40 <= player.x) {
+        player.resetPlayer();
+      }
     }
   }
-	
 
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 };
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+   
+// Draw the enemy on the screen, required method for game
+
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y) {
-  this.x = x;
-  this.y = y;
-  this.sprite = 'images/char-boy.png';
-}
 
-Player.prototype.update = function (dt) {
-  if (this.y < 50) {
-    this.resetPlayer();
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y; 
+    this.sprite = 'images/char-boy.png'
   }
 
-  
-}
-
-Player.prototype.render = function () {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
-Player.prototype.handleInput = function (keyPressed) {
-  if (keyPressed === 'left' && this.x > 33) {
-    this.x -= 100;
-  }
-  else if (keyPressed === 'up' && this.y > 18) {
-    this.y -= 83;
-  }
-  else if (keyPressed === 'right' && this.x < 400) {
-    this.x += 100;
-  }
-  else if (keyPressed === 'down' && this.y < 380) {
-    this.y += 83;
+  update(dt) {
+    if (this.y < 30) {
+      this.resetPlayer();
+    }
   }
 
-}
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 
-Player.prototype.resetPlayer = function () {
-  this.x = 200;
-  this.y = 405;
+  handleInput(keyPressed) {
+    if (keyPressed === 'left' && this.x > 33) {
+      this.x -= 100;
+    }
+    else if (keyPressed === 'up' && this.y > 18) {
+      this.y -= 83;
+    }
+    else if (keyPressed === 'right' && this.x < 400) {
+      this.x += 100;
+    }
+    else if (keyPressed === 'down' && this.y < 380) {
+      this.y += 83;
+    }
+  }
+
+  resetPlayer() {
+    this.x = 200;
+    this.y = 405;
+  }
 }
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
+
 // Place the player object in a variable called player
 let player = new Player(200, 405);
 
-
+// Place all enemy objects in an array called allEnemies
 let allEnemies = [new Enemy(0, 73, 1), new Enemy(-4, 73, 2), new Enemy(-8, 156, 1.5), new Enemy(-6, 239, 1.6)];
 
 
