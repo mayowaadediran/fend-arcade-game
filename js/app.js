29cd5bdd488370b6a634d30e4a415bb90e72ac9b
Mayowa Adediran <ddrn_mayowa@yahoo.com>
2019-01-10 15:15:58 +0100

    add start game function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+let welcomeModal = document.getElementById('welcome-modal');
+let overlay = document.querySelector(".overlay")
+
+function startGame() {
+  welcomeModal.style.display = "none";
+  overlay.classList.add("hide");
+}
+
+
 class Enemy  {
   constructor(x, y, speed) {
   this.x = x;
@@ -25,19 +34,6 @@ class Enemy  {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 };
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-   
-// Draw the enemy on the screen, required method for game
-
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 class Player {
   constructor(x, y) {
